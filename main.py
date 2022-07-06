0ab40eea7e8870587cc2e790bf308738de99ac7c
MistyIce <67894395+MistyIce672@users.noreply.github.com>
2022-07-06 12:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for line in lines:
 	rand3 = random. choice(string. ascii_letters)
 	rand4 = random. choice(string. ascii_letters)
 	count += 1
-	url = 'https://ngl.link/_hetha.iii'
+	url = 'link here'
 	myobj = {"question":line,"deviceId":f"b2a007d0-5574-43b6-9e0a-4e3d4dc{rand1}{rand2}{rand3}{rand4}"}
 	x = requests.post(url, json = myobj)
 	print(f"b2a007d0-5574-43b6-9e0a-4e3d4dc{rand1}{rand2}{rand3}{rand4}")
