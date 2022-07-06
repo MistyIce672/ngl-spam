e511ad6b859c1950095f24920f0428d0aed773af
MistyIce <67894395+MistyIce672@users.noreply.github.com>
2022-07-06 12:07:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import string, random
+from time import sleep
+rand1 = random. choice(string. ascii_letters)
+rand2 = random. choice(string. ascii_letters)
+rand3 = random. choice(string. ascii_letters)
+rand4 = random. choice(string. ascii_letters)
+with open("txt.txt","r") as f:
+	lines = f.readlines()
+count = 0
+for line in lines:
+	rand1 = random. choice(string. ascii_letters)
+	rand2 = random. choice(string. ascii_letters)
+	rand3 = random. choice(string. ascii_letters)
+	rand4 = random. choice(string. ascii_letters)
+	count += 1
+	url = 'https://ngl.link/_hetha.iii'
+	myobj = {"question":line,"deviceId":f"b2a007d0-5574-43b6-9e0a-4e3d4dc{rand1}{rand2}{rand3}{rand4}"}
+	x = requests.post(url, json = myobj)
+	print(f"b2a007d0-5574-43b6-9e0a-4e3d4dc{rand1}{rand2}{rand3}{rand4}")
+	print(x)
+	print(count)
+	sleep(10)
+
